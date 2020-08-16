21512dc6d1166c436ca312a8352b4ca28a63eaa8
Anna Nguyen <axn3@cornell.edu>
2020-08-15 23:43:30 -0400

    added drawer

--- flowerish-app/src/screens/homeScreen/index.js
+++ flowerish-app/src/screens/homeScreen/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Typography} from '@material-ui/core';
+import React, {useState} from 'react';
+import {Typography, Drawer, Button} from '@material-ui/core';
 import waves from '../../assets/waves.png';
 
 const styles = {
@@ -7,6 +7,9 @@ const styles = {
     height: '100vh',
     display: 'flex',
     flexDirection: 'column',
+  },
+  nav: {
+    backgroundColor: '#EBF3F5',
   },
   waves: {
     backgroundColor: '#EBF3F5',
@@ -18,17 +21,35 @@ const styles = {
 }
 
 export const HomeScreen = () => {
+  const [open, setOpen] = useState(false);
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setOpen(open);
+  };
+
+  const list = (anchor) => (
+    <div onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
+      <Typography>hello</Typography>
+        <Typography>hello</Typography>
+        <Typography>hello</Typography>
+    </div>
+  )
+
   return(
     <div style={styles.home}>
-      <div>
-
+      <div style={styles.nav}>
+        <Button onClick={toggleDrawer(true)}>NAV</Button>
+        <Drawer anchor='right' open={open} onClose={toggleDrawer(false)}>
+          {list(open)}
+        </Drawer>
+        
       </div>
       <img src={waves} style={styles.waves}/>
       <div style={styles.body}>
-        <Typography>what</Typography>
+        
       </div>
-</div>
-    
-
+    </div>
   );
 };
